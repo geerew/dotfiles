e3355a40459a73a82a63350767c9f0244e1777ac
Michael Bell <michaelrbell@ymail.com>
2025-06-25 20:45:47 +0000

    Create wg-toggle.sh

---
+++ .config/wg-toggle.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# $ sudo pacman -Syu kdialog
+# $ sudo visudo -f /etc/sudoers.d/wireguard
+# USERNAME ALL=(root) NOPASSWD: \
+#    /usr/bin/wg-quick up se-mma-wg-004, \
+#    /usr/bin/wg-quick down se-mma-wg-004
+
+IF="se-mma-wg-004"
+
+connected_interface=$(networkctl | grep -P "\d+ .* wireguard routable" -o | cut -d" " -f2)
+
+# Figure out current state & desired action
+if [[ $connected_interface ]]; then 
+    ACTION="down"
+    QUESTION="WireGuard is up. Disable it?"
+else
+    ACTION="up"
+    QUESTION="WireGuard is down. Enable it?"
+fi
+
+# Ask the user via KDE’s native dialog
+if kdialog --yesno "$QUESTION"; then
+    sudo wg-quick "$ACTION" "$IF"
+    # Optional: send a desktop notification of success/failure
+    if [[ $? -eq 0 ]]; then
+      kdialog --passivepopup "WireGuard ${ACTION} successful" 2
+    else
+      kdialog --error "Failed to ${ACTION} WireGuard"
+    fi
+fi
