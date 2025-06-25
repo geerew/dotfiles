259a6acf024ce30715a1c60b9e2f02f6dd8ec7d6
Michael Bell <michaelrbell@ymail.com>
2025-06-25 20:52:33 +0000

    Update wg-status.sh

--- .config/wg-status.sh
+++ .config/wg-status.sh
@@ -3,7 +3,7 @@
 connected_interface=$(networkctl | grep -P "\d+ .* wireguard routable" -o | cut -d" " -f2)
 
 if [[ $connected_interface ]]; then 
-    echo -e "%{F#00FF00}●%{F-}"
+    echo -e "%{F#F0C674}VPN %{F-}%{F#00FF00}●%{F-}"
 else 
-    echo -e "%{F#FF0000}●%{F-}"
+    echo -e "%{F#F0C674}VPN %{F-}%{F#FF0000}●%{F-}"
 fi
